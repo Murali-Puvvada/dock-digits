9019d5629eb973ebaef894e9e2e3ce990710791f
Murali Puvvada <saipuvvada81299@gmail.com>
2026-03-06 17:34:37 +0100

    feat: MUR-10 Prettier

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [
+    "type-enum": [
       2,
-      'always',
-      ['feat', 'fix', 'chore', 'docs', 'refactor', 'test', 'style'],
+      "always",
+      ["feat", "fix", "chore", "docs", "refactor", "test", "style"],
     ],
-    'type-empty': [2, 'never'],
-    'subject-case': [0, 'never'],
-    'header-max-length': [2, 'always', 72],
+    "type-empty": [2, "never"],
+    "subject-case": [0, "never"],
+    "header-max-length": [2, "always", 72],
   },
 };
